5374ce1d7eb6b574dcefaf8f39d20cb77155fe6e
Helen <204595204+es-hp@users.noreply.github.com>
2026-08-21 14:08:22 -0400

    WIP: project images carousel in project slides

--- src/components/projects/ImageCarousel.tsx
+++ src/components/projects/ImageCarousel.tsx
@@ -1,28 +1,23 @@
-import useEmblaCarousel from 'embla-carousel-react';
-
 import { type ProjectImage } from '@/types/types';
+
+import { Carousel } from '../carousel/Carousel';
 
 type ImageCarouselProps = {
   projectImages: ProjectImage[];
 };
 
 export function ImageCarousel({ projectImages }: ImageCarouselProps) {
-  const [emblaRef, emblaApi] = useEmblaCarousel({
-    loop: true,
-    watchDrag: () => {
-      return window.matchMedia('(any-pointer: coarse)').matches;
-    },
-  });
+  const content = projectImages.map((image, index) => (
+    <div key={index}>
+      <img src={image.src} alt={image.alt} />
+    </div>
+  ));
+
+  const options = {};
 
   return (
-    <div ref={emblaRef} className="carousel-viewport">
-      <div className="carousel-container">
-        {projectImages.map((image, index) => (
-          <div key={index}>
-            <img src={image.src} alt={image.alt} />
-          </div>
-        ))}
-      </div>
-    </div>
+    <>
+      <Carousel carouselContent={content} options={options} />
+    </>
   );
 }
