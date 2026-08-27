9c71d70db05877b095da6e34953e7e2356d10605
Helen <204595204+es-hp@users.noreply.github.com>
2026-08-27 13:57:44 -0400

    refactor(AppLayout): make it configurable with optional padding and/or footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import { Route, Routes } from 'react-router-dom';
 
-import Layout from '@/components/layout/Layout';
-
+import { AppLayout } from './components/layout/Layout';
 import { Home } from './pages/home/Home';
 import { ProjectGallery } from './pages/projects/ProjectGallery';
 import { Projects } from './pages/projects/Projects';
@@ -10,9 +9,31 @@ function App() {
   return (
     <>
       <Routes>
-        <Route element={<Layout />}>
+        <Route
+          element={
+            <AppLayout
+              horizPadding={true}
+              topPadding={true}
+              botPadding={true}
+              footer={false}
+            />
+          }
+        >
           <Route path="/" element={<Home />} />
+        </Route>
+        <Route
+          element={
+            <AppLayout
+              horizPadding={true}
+              topPadding={true}
+              botPadding={true}
+              footer={true}
+            />
+          }
+        >
           <Route path="/projects" element={<Projects />} />
+        </Route>
+        <Route element={<AppLayout />}>
           <Route path="/projects/:projectId" element={<ProjectGallery />} />
         </Route>
       </Routes>
--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,9 +1,35 @@
 import { Outlet } from 'react-router-dom';
 
-export default function AppLayout() {
+import clsx from 'clsx';
+
+import { Footer } from './Footer';
+import styles from './Layout.module.css';
+
+type AppLayoutProps = {
+  horizPadding?: boolean;
+  topPadding?: boolean;
+  botPadding?: boolean;
+  footer?: boolean;
+};
+
+export function AppLayout(props: AppLayoutProps) {
+  const {
+    horizPadding = false,
+    topPadding = false,
+    botPadding = false,
+    footer = true,
+  } = props;
   return (
-    <div className="app-layout">
+    <div
+      className={clsx(
+        styles.appLayout,
+        horizPadding && styles['appLayout--h-padding'],
+        topPadding && styles['appLayout--top-padding'],
+        botPadding && styles['appLayout--bot-padding']
+      )}
+    >
       <Outlet />
+      {footer && <Footer />}
     </div>
   );
 }
