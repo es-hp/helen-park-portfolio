be00d045cd9f433051670ecbf338062728eadd30
Helen <204595204+es-hp@users.noreply.github.com>
2026-09-12 15:04:09 -0400

    fixup! feat(AppLayoutRefs): add context and hooks for calculating available window height

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,8 +1,12 @@
+import { useAppLayoutRefs } from '@/hooks/useAppLayoutRefs';
+
 export const FOOTER_HEIGHT_REM: number = 4;
 
 export function Footer({ heightRem }: { heightRem: number }) {
+  const { footerRef } = useAppLayoutRefs();
   return (
     <footer
+      ref={footerRef}
       className="footer flex items-center justify-between w-full max-w-3xl px-(--app-layout-padding)"
       style={{ height: `${heightRem}rem` }}
     >
--- src/components/projects/ProjectGalleryHeader.tsx
+++ src/components/projects/ProjectGalleryHeader.tsx
@@ -1,12 +1,14 @@
 import { Link } from 'react-router-dom';
 
 import { BackButton } from '@/components/ui/BackButton';
+import { useAppLayoutRefs } from '@/hooks/useAppLayoutRefs';
 
 import styles from './ProjectComponents.module.css';
 
 export function ProjectGalleryHeader() {
+  const { headerRef } = useAppLayoutRefs();
   return (
-    <header className={styles.carouselHeader}>
+    <header ref={headerRef} className={styles.carouselHeader}>
       <BackButton />
       <h1>Projects</h1>
       <Link to="/Resume">Resume</Link>
