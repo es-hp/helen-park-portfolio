e672b6492cddbbab304ce4b4b8fa3fa61324ed48
Helen <204595204+es-hp@users.noreply.github.com>
2026-09-16 15:43:46 -0400

    feat(Projects): add page layout (dynamic)

--- src/pages/projects/Projects.tsx
+++ src/pages/projects/Projects.tsx
@@ -1,27 +1,33 @@
 import { Link } from 'react-router-dom';
 
+import { Spinner } from '@/components/ui/LoadingSpinner';
 import { useProjects } from '@/hooks/useProjects';
 
 export function Projects() {
   const { data: projects, isPending, isError } = useProjects();
 
+  // const isTrue: boolean = true;
+
   return (
-    <main className="projects border border-red-600">
-      <div>
-        <h1>Projects</h1>
-        <div className="flex flex-col gap-4 justify-center">
+    <main className="projects flex flex-col items-center justify-start border border-red-600">
+      <header>
+        <h1 className="text-4xl">Projects</h1>
+      </header>
+      <div className="flex-1 flex flex-col-reverse justify-center md:flex-row items-start w-full max-w-3xl gap-16 py-32 border border-blue-300">
+        <div className="projects-list flex-1 flex flex-col gap-6 self-stretch md:min-h-80 border border-amber-400">
           {isPending ? (
-            <div>Loading...</div>
+            <Spinner />
           ) : isError ? (
             <div>Error loading projects.</div>
           ) : (
-            projects.map((project, index) => (
+            projects?.map((project, index) => (
               <Link to={`/projects/${project.id}`} key={index}>
                 {project.title}
               </Link>
             ))
           )}
         </div>
+        <div className="projects-filter-container flex flex-col w-full md:w-48 h-32 md:min-h-82 border border-green-300"></div>
       </div>
     </main>
   );
