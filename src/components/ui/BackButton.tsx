69d7eac37c357b2eda7167cfad96adf0130aefe7
Helen <204595204+es-hp@users.noreply.github.com>
2026-09-17 16:44:17 -0400

    feat(BackButton): add props support to BackButton component

--- src/components/ui/BackButton.tsx
+++ src/components/ui/BackButton.tsx
@@ -1,10 +1,12 @@
+import type { ComponentPropsWithoutRef } from 'react';
 import { useNavigate } from 'react-router-dom';
 
-export function BackButton() {
+export function BackButton(props: ComponentPropsWithoutRef<'button'>) {
   const navigate = useNavigate();
 
   return (
     <button
+      {...props}
       onClick={() => {
         if (window.history.length > 1) {
           void navigate(-1);
